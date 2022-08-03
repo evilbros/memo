c879f9433ee1f60c52d185677a098670e0f4bbff
evil <evil@x.com>
2022-08-03 22:50:11 +0800

    added web sync script for deployment

---
+++ deploy/web/sync.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+#########################################
+# name & arg
+projects=(
+    "pushbox        play"
+    "tinydraw"
+)
+#########################################
+
+[ ! $1 ] && echo "$0 name" && exit 1
+
+# check name
+found=0
+for p in "${projects[@]}"; do
+    name=${p%% *}
+    arg=${p##* }
+
+    if [ $1 == $name ]; then
+        found=1
+        break
+    fi
+done
+
+[ $found -eq 0 ] && echo "invalid name" && exit 1
+
+# deploy
+DIR=$(mktemp -d $name.XXXX)
+cd $DIR
+
+git clone git@192.168.0.200:$name
+cd $name
+
+rm -rf package.json package-lock.json
+./dev.sh build $arg
+tarfile=$(./dev.sh tar $arg)
+
+rm -rf ../../../game/$name
+mkdir -p ../../../game/$name
+tar -C ../../../game/$name -xf $tarfile
+
+cd ../..
+rm -rf $DIR
+
+# done
+echo "$name deployed successfully"
