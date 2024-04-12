e0f302ebaf4d83ac5f26c8c97f03ed3224f22875
evil <evil@x.com>
2024-04-12 15:45:42 +0800

    updated textadept settings

--- settings/textadept/init.lua
+++ settings/textadept/init.lua
@@ -1,11 +1,21 @@
-view:set_theme('light', {font = 'Microsoft YaHei', size = 12})
+local function shift_del()
+    if buffer.selection_empty then
+        buffer.line_cut()
+    else
+        buffer.cut()
+    end
+end
+
+----------------------------------------------------------------------
+
+view:set_theme('base16-monokai', {font = 'Microsoft YaHei', size = 12})
 
 buffer.use_tabs = false
 buffer.tab_width = 4
 buffer.eol_mode = buffer.EOL_LF
 
 keys['ctrl+,'] = textadept.menu.menubar['Edit/Preferences'][2]
-keys['shift+del'] = buffer.line_delete
+keys['shift+del'] = shift_del
 keys['alt+up'] = buffer.move_selected_lines_up
 keys['alt+down'] = buffer.move_selected_lines_down
 keys['alt+z'] = textadept.menu.menubar['View/Toggle Wrap Mode'][2]
