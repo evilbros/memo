36e608d8de1c015b2837e87398e1da5523e9926d
lg <lg@x.com>
2025-03-25 18:33:09 +0800

    ddns: dnspod changed to api v3

--- deploy/ddns/dnspod.js
+++ deploy/ddns/dnspod.js
@@ -1,8 +1,11 @@
 #!/usr/bin/env node
 
-require('./lib/ext');
+const { dnspod } = require("tencentcloud-sdk-nodejs-dnspod");
+const { Client } = require("tencentcloud-sdk-nodejs-dnspod/tencentcloud/services/dnspod/v20210323/dnspod_client");
 
-var axios = require('axios');
+const DnsClient = dnspod.v20210323.Client;
+
+// ============================================================================
 
 var config;
 try {
@@ -13,12 +16,94 @@ try {
 
 // ============================================================================
 
+/**@type {Client} */
+var client;
+function getClient() {
+    if (!client) {
+        client = new DnsClient({
+            credential: {
+                secretId: config.dnspod.secret_id,
+                secretKey: config.dnspod.secret_key,
+            },
+        });
+    }
+
+    return client;
+}
+
+// ============================================================================
+
+async function listRecords(recordType) {
+    try {
+        let c = getClient();
+        const r = await c.DescribeRecordList({
+            Domain: config.dnspod.domain,
+            RecordType: recordType,
+        });
+        console.log(r);
+    } catch (err) {
+        console.error("listRecords error:", err);
+    }
+}
+
+async function modifyRecord(recordId, subDomain, recordType, val) {
+    try {
+        let c = getClient();
+        const r = await c.ModifyRecord({
+            Domain: config.dnspod.domain,
+            RecordId: recordId,
+            SubDomain: subDomain,
+            RecordType: recordType,
+            RecordLine: "默认",
+            Value: val,
+            TTL: 600,
+        });
+        console.log('OK')
+    } catch (err) {
+        console.error("modifyRecordInList error:", err);
+    }
+}
+
+async function createRecord(subDomain, recordType, val) {
+    try {
+        let c = getClient();
+        const r = await c.CreateRecord({
+            Domain: config.dnspod.domain,
+            SubDomain: subDomain,
+            RecordType: recordType,
+            RecordLine: "默认",
+            Value: val,
+            TTL: 600,
+        });
+        console.log('OK. RecordId=', r.RecordId);
+    } catch (err) {
+        console.error("createRecord error:", err);
+    }
+}
+
+async function deleteRecord(recordId) {
+    try {
+        let c = getClient();
+        const r = await c.DeleteRecord({
+            Domain: config.dnspod.domain,
+            RecordId: recordId,
+        });
+        console.log('OK');
+    } catch (err) {
+        console.error("deleteRecord error:", err);
+    }
+}
+
+// ============================================================================
+
 function usage() {
     console.log("args: cmd [args...]");
     console.log("");
     console.log("cmd:");
-    console.log("   list            list all 'A' records");
-    console.log("   update ip       update configured records to point to 'ip'");
+    console.log("   list [type]                     list all records of 'type'");
+    console.log("   update ip                       update configured records to point to 'ip'");
+    console.log("   create subdomain type val       create a subdomain of 'type' and 'val'");
+    console.log("   delete recordid                 delete a record with 'recordid'");
     console.log("");
     process.exit(1);
 }
@@ -31,59 +116,44 @@ if (args.length < 1) {
 var cmd = args[0];
 args = args.slice(1);
 
-// ============================================================================
-
-async function rec_list() {
-    let {data} = await axios.post('https://dnsapi.cn/Record.List', {
-        login_token: config.dnspod.api_key,
-        format:      'json',
-        domain:      'ioof.top',
-        record_type: 'A',
-    }, {
-        headers: {
-            'User-Agent': 'my ddns/1.0.0 (x@x.com)',
-        },
-    });
-
-    console.log(data);
-}
-
-async function rec_update(rec, ip) {
-    let {data} = await axios.post('https://dnsapi.cn/Record.Modify', {
-        login_token: config.dnspod.api_key,
-        format:      'json',
-        domain:      'ioof.top',
-        record_id:   rec.id,
-        sub_domain:  rec.sub_domain,
-        record_type: 'A',
-        record_line: '默认',
-        value:       ip,
-    }, {
-        headers: {
-            'User-Agent': 'my ddns/1.0.0 (x@x.com)',
-        },
-    });
-
-    console.log(data);
-}
-
-// ============================================================================
-
 (async () => {
     switch (cmd) {
         case 'list':
-            await rec_list();
+            let tp = args[0];
+
+            await listRecords(tp);
             break;
 
         case 'update':
-            let ip = args[0];
-            if (!ip) usage();
+            {
+                let ip = args[0];
+                if (!ip) usage();
 
-            for (let rec of config.dnspod.records) {
-                await rec_update(rec, ip);
-                await sleep(1000);
+                for (let rec of config.dnspod.records) {
+                    await modifyRecord(rec.id, rec.sub_domain, 'A', ip);
+                    await sleep(1000);
+                }
             }
+            break;
 
+        case 'create':
+            {
+                let subDomain = args[0];
+                let tp = args[1];
+                let val = args[2];
+                if (!val) usage();
+
+                await createRecord(subDomain, tp, val);
+            }
+            break;
+
+        case 'delete':
+            {
+                let recordId = args[0];
+                if (!recordId) usage();
+
+                await deleteRecord(recordId);
+            }
             break;
     }
 })().catch(console.error);
